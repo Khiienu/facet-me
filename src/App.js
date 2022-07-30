e858c8987b8b264445c023f10f425ab3258c40bc
Khiienu <anthonyhuynh123@gmail.com>
2022-07-30 15:32:10 -0500

    Added incrementer and decrementer counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import './App.css';
+import { React, useState } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(75)
+
+    //* if the person *hits* then the percentage goes down by 10. 
+    const hit = () => {
+        if (counter === 25 ){
+          setCounter(count => count+0)
+        }
+        else{
+        setCounter(count => count-10 )
+        }
+    };
+
+    //! if the person *misses* then the percentage goes up by 10.
+    const miss = () => {
+        if (counter === 75 ){
+          setCounter(count => count+0)
+        }
+        else {
+        setCounter(count => count+10)
+        }
+    }
+    
+
+
+
+
+
+
   return (
     <div className="App">
-        <h1>This is the start of my facet me website</h1> 
+      {counter}
+      <div className="engravings-pos1">
+        positive engraving 1
+        <div className="buttons">
+          <button className="button" onClick={hit}> + </button>
+          <button className="button" onClick={miss}> - </button>
+        </div>
+      </div>
+      <div className="engravings-pos2">
+        positive engraving 2
+        <div className="buttons">
+          <button className="button" onClick={hit}> + </button>
+          <button className="button" onClick={miss}> - </button>
+        </div>
+      </div>
+      <div className="engravings-neg1">
+        negative engraving 
+        <div className="buttons">
+          <button className="button" onClick={hit}> + </button>
+          <button className="button" onClick={miss}> - </button>
+        </div>
+      </div>
+      <div className="toolbar">
+        this is tool bar
+      </div>
     </div>
   );
 }
---
+++ src/components/HitOrMiss/HitOrMiss.js
@@ -0,0 +1,15 @@
+// import { React, useState } from 'react'
+
+// export default function nodes(){
+//     const [counter, setCounter] = useState(0)
+
+//     //* if the person *hits* then the percentage goes down by 10. 
+//     const hit = () => {
+//         setCounter(count => count -10 )
+//     };
+
+//     //! if the person *misses* then the percentage goes up by 10.
+//     const miss = () => {
+//         setCounter(count => +10)
+//     }
+// }
